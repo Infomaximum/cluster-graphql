f5af5c7023a3b93eff2a9f56adfb8e816143c21b
Vladimir A. Ulitin <ulitin@infomaximum.com>
2018-04-03 08:50:18 +0300

    fix increment version: 0.0.23RC28

---
+++ src/main/java/com/infomaximum/cluster/graphql/exception/GraphQLExecutorInvalidSyntaxException.java
@@ -0,0 +1,9 @@
+package com.infomaximum.cluster.graphql.exception;
+
+public class GraphQLExecutorInvalidSyntaxException extends GraphQLExecutorException {
+
+    public GraphQLExecutorInvalidSyntaxException() {
+        super();
+    }
+
+}
