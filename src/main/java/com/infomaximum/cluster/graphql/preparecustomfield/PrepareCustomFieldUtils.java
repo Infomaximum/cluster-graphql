8ca9b73ee531b9e56b2bc82cce7f29d61241f418
Vladimir A. Ulitin <ulitin@infomaximum.com>
2018-04-06 18:41:20 +0300

    fix increment version: 0.0.23RC32

--- src/main/java/com/infomaximum/cluster/graphql/preparecustomfield/PrepareCustomFieldUtils.java
+++ src/main/java/com/infomaximum/cluster/graphql/preparecustomfield/PrepareCustomFieldUtils.java
@@ -1,6 +1,5 @@
 package com.infomaximum.cluster.graphql.preparecustomfield;
 
-import com.infomaximum.cluster.graphql.exception.GraphQLExecutorInvalidSyntaxException;
 import graphql.language.Field;
 import graphql.language.SourceLocation;
 import graphql.schema.DataFetchingEnvironment;
@@ -10,9 +9,6 @@ public class PrepareCustomFieldUtils {
     public static String getKeyField(DataFetchingEnvironment dataFetchingEnvironment) {
         if (dataFetchingEnvironment.getFields().size() < 1) {
             throw new RuntimeException("Not support zero field");
-        } else if (dataFetchingEnvironment.getFields().size() > 1) {
-            String fieldName = dataFetchingEnvironment.getFields().get(0).getName();
-            throw new GraphQLExecutorInvalidSyntaxException("Multi fields: " + fieldName);
         }
         Field field = dataFetchingEnvironment.getFields().get(0);
 
