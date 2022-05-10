6032e6a5503729a9cdf37188ac8dac24ada0a84c
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-05-10 13:23:57 +0300

    build: 1.1.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,5 @@ module com.infomaximum.cluster.graphql {
     requires com.google.common;
     requires graphql.java;
     requires java.validation;
-    requires org.slf4j;
     requires org.reflections;
 }
