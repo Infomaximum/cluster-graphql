5076b1b77d240f5f288090ecaa63912a54e5fe8e
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-08-18 09:04:15 +0300

    build: 1.1.1f3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module com.infomaximum.cluster.graphql {
     requires org.reflections.reflections;
     requires org.checkerframework.checker.qual;
 
-    exports com.infomaximum.cluster.graphql.remote.graphql.executor;
     exports com.infomaximum.cluster.graphql.struct;
     exports com.infomaximum.cluster.graphql.executor.subscription;
     exports com.infomaximum.cluster.graphql.subscription;
@@ -23,4 +22,7 @@ module com.infomaximum.cluster.graphql {
     exports com.infomaximum.cluster.graphql.preparecustomfield;
     exports com.infomaximum.cluster.graphql.anotation;
     exports com.infomaximum.cluster.graphql.fieldargument.custom;
+
+    exports com.infomaximum.cluster.graphql.remote.graphql.executor to com.infomaximum.cluster, com.infomaximum.platform;
+    exports com.infomaximum.cluster.graphql.remote.graphql.subscribe to com.infomaximum.cluster;
 }
