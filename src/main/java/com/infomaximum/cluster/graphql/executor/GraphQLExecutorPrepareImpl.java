e8fcd6cee20e7d2a6d494bf1f28754a22cb6f5fb
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-08-18 17:51:53 +0300

    build: 1.1.1f6

--- src/main/java/com/infomaximum/cluster/graphql/executor/GraphQLExecutorPrepareImpl.java
+++ src/main/java/com/infomaximum/cluster/graphql/executor/GraphQLExecutorPrepareImpl.java
@@ -32,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
